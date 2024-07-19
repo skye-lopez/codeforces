0f48d6260c45df19012e861facb5b93a12732962
skye-lopez <sky.lopez.dev@gmail.com>
2024-07-18 20:56:32 -0700

    draft

---
+++ A_cAPS-lOCK.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // if str[i] is lower and str[i+1-n] are upper yes ["z"] counts too
+    // if str[i-n] are all upper
+    string str; cin >> str;
+    char a = 'a';
+    lower = tolower(a)
+    for(int i=0; i<str.size(); ++i) {
+    }
+}
---
+++ A_domino-piling.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int m; cin>>m;
+    int n; cin>>n;
+    cout << floor((m*n)/2);
+}
